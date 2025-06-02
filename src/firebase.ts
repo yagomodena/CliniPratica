7635495886e238e94ce41b5c910aea81cf952e2c
Yago Modena <yagobmodena1@gmail.com>
2025-06-02 20:47:11 +0000

    percebi que conectou o saas no projeto errado do firebase, o projeto que eu quero que conecte é esse: ID do projeto clinipratica-d5519 Número do projeto 551870373506 Chave de API da Web AIzaSyCoqOk3q5nfNRDL1tY2abLlTa5nhqq1TDU NÃO ESSE: ID do projeto: clinipratica - Número do projeto:123633772529

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,7 +12,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCoqOk3q5nfNRDL1tY2abLlTa5nhqq1TDU",
   authDomain: "clinipratica-d5519.firebaseapp.com",
   projectId: "clinipratica-d5519",
-  storageBucket: "clinipratica-d5519.appspot.com", // Corrected storageBucket name
+  storageBucket: "clinipratica-d5519.appspot.com",
   messagingSenderId: "551870373506",
   appId: "1:551870373506:web:201dad4f2fad73e6c5de9b",
   measurementId: "G-1QXWLN3487"
@@ -27,3 +27,4 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, db, storage };
+
